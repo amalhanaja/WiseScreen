04141ce1912ced8c7b294e3a02c71ddcca1cdfc8
amalhanaja <amalhanaja@gmail.com>
2023-01-11 22:18:51 +0700

    [FEAT]: init stacked bar chart with legend component

---
+++ app/src/main/java/com/amalcodes/wisescreen/presentation/components/StackedBarChartWithLegend.kt
@@ -0,0 +1,79 @@
+package com.amalcodes.wisescreen.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+
+@Immutable
+data class StackedBarChartWithLegendComponentState(
+    val data: List<StackedBarChartWithLegendItem>,
+)
+
+@Immutable
+data class StackedBarChartWithLegendItem(
+    val percentage: Float,
+    val color: Color,
+    val label: String,
+    val description: String,
+)
+
+@Composable
+fun StackedBarChartWithLegendComponent(
+    modifier: Modifier,
+    state: StackedBarChartWithLegendComponentState,
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            state.data.forEach { item ->
+                Box() {
+
+                }
+            }
+        }
+        Row(modifier = Modifier.fillMaxWidth()) {
+        }
+    }
+}
+
+@Composable
+private fun StackedBarChartBar(item :StackedBarChartWithLegendItem, modifier: Modifier){
+
+}
+
+@Composable
+private fun StackedBarChartLegend(
+    item: StackedBarChartWithLegendItem,
+    modifier: Modifier = Modifier,
+) {
+    ConstraintLayout(modifier = modifier) {
+        val (labelRef, indicatorRef, descriptionRef) = createRefs()
+        Box(modifier = Modifier.constrainAs(ref = indicatorRef) {
+            width = Dimension.value(8.dp)
+            height = Dimension.value(8.dp)
+            start.linkTo(parent.start)
+            top.linkTo(labelRef.top)
+            centerVerticallyTo(labelRef)
+        }) {
+
+        }
+        Text(text = item.label, modifier = Modifier.constrainAs(ref = labelRef) {
+            width = Dimension.fillToConstraints
+            start.linkTo(indicatorRef.start, 8.dp)
+            end.linkTo(parent.end)
+            top.linkTo(parent.top)
+        })
+        Text(text = item.description)
+    }
+}
