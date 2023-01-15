1a37d57dc191707c92b2ce9225c79150315002d9
amalhanaja <amalhanaja@gmail.com>
2023-01-15 16:21:55 +0700

    [FEAT]: dynamic color

--- app/src/main/java/com/amalcodes/wisescreen/presentation/foundation/AppTheme.kt
+++ app/src/main/java/com/amalcodes/wisescreen/presentation/foundation/AppTheme.kt
@@ -1,11 +1,32 @@
 package com.amalcodes.wisescreen.presentation.foundation
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.material.color.DynamicColors
 
 @Composable
 fun AppTheme(
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
-    MaterialTheme(content = content)
+    val isDark = isSystemInDarkTheme()
+    val isDynamicColorAvailable = DynamicColors.isDynamicColorAvailable()
+    val context = LocalContext.current
+    MaterialTheme(
+        colorScheme = when {
+            isDynamicColorAvailable && isDark -> dynamicDarkColorScheme(context)
+            isDynamicColorAvailable -> dynamicLightColorScheme(context)
+            else -> lightColorScheme(
+                primary = ColorPalettes.PersianGreen,
+                primaryContainer = ColorPalettes.PersianGreen
+            )
+        },
+        content = content,
+        typography = MaterialTheme.typography.copy()
+    )
 }
---
+++ app/src/main/java/com/amalcodes/wisescreen/presentation/foundation/ColorPalettes.kt
@@ -0,0 +1,11 @@
+package com.amalcodes.wisescreen.presentation.foundation
+
+import androidx.compose.ui.graphics.Color
+import javax.annotation.concurrent.Immutable
+
+@Immutable
+object ColorPalettes {
+    val PersianGreen: Color = Color(0xFF2A9D8F)
+    val OrangeYellowCrayola: Color = Color(0xFFE9C46A)
+    val BurntSienna: Color = Color(0xFFE76F51)
+}
